11145e5981f9a2500b5b027c2472b5055b26a591
Einar Ingebrigtsen <einar@dolittle.com>
2017-08-08 23:37:25 +0200

    Putting in doLittle extend plugin

--- gulp/tasks/babel.js
+++ gulp/tasks/babel.js
@@ -3,14 +3,13 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import babel from "gulp-babel";
+import doLittleExtend from "../../babel-plugin-dolittle-extend";
 
 export default () => {
     return babel({
             presets: ["es2015"],
             plugins: [
-                //"./../../babel-plugin-dolittle-constructors.js"
-                //"/Users/einari/Projects/dolittle/Samples/Basic/Source/Web/babel-plugin-dolittle-extend.js"
-                //"C:\\Projects\\dolittle\\Samples\\Basic\\Source\\Web\\babel-plugin-dolittle-extend.js"
+                doLittleExtend
             ],
             sourceMaps: true,
             //sourceRoot: "../"
